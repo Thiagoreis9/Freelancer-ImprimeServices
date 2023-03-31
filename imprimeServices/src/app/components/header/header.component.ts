4e78203269788c1fcf9ed4ba22084b8be35b1ed1
Thiago Reis <reist36@gmail.com>
2023-03-31 00:26:59 -0300

    Creating the footer layout

--- imprimeServices/src/app/components/header/header.component.ts
+++ imprimeServices/src/app/components/header/header.component.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    header = document.getElementById('header');
+    headerMenu= document.getElementById('headerMenu');
+    content = document.getElementById('content');
+    showSidebar = false;
 
-  ngOnInit(): void {
-  }
+    constructor(){
+
+    }
+
+    toggleSideBar(){
+      this.showSidebar = !this.showSidebar;
+
+      if(this.showSidebar){
+        this.headerMenu!.style.marginLeft = '-5vw';
+        this.headerMenu!.style.animationName = 'showSideBar';
+        this.content!.style.filter = 'blur(2px)';
+      } else{
+        this.headerMenu!.style.marginLeft = '-100vw';
+        this.headerMenu!.style.animationName = 'closeSideBar';
+        this.content!.style.filter = '';
+
+      }
+
+    }
+
+    closeSideBar(){
+      if(this.showSidebar){
+        this.toggleSideBar();
+      }
+    }
+
+
+
+    ngOnInit(): void {
+      this.header = document.getElementById('header');
+      this.headerMenu= document.getElementById('headerMenu');
+      this.content = document.getElementById('content');
+      this.showSidebar = false;
+
+
+      /*window.addEventListener('resize', function(event) {
+        if(window.innerWidth > 1039){
+          this.closeSidebar()
+        }
+      });*/
+
+
+    }
+
+
 
 }
