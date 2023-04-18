24cca71f5720afe820ae41e4d76086cd93e7f6dc
Thiago Reis <reist36@gmail.com>
2023-04-17 23:21:37 -0300

    adding remaining stores contact page layout

--- imprimeServices/src/app/app-routing.module.ts
+++ imprimeServices/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreTGSComponent } from './storesDetails/store-tgs/store-tgs.component';
 import { HeaderComponent } from './components/header/header.component';
+import { StorePlazaComponent } from './storesDetails/store-plaza/store-plaza.component';
+import { StorePistaoComponent } from './storesDetails/store-pistao/store-pistao.component';
 
 const routes: Routes = [
     { path: '', component: HeaderComponent },
-    { path: 'storeTGS', component: StoreTGSComponent }
+    { path: 'storeTGS', component: StoreTGSComponent },
+    { path: 'storePlaza', component: StorePlazaComponent },
+    { path: 'storePistao', component: StorePistaoComponent }
 
 ];
 
