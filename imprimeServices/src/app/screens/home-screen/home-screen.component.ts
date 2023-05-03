068c419830a01c9c47521e3354f24726b3e87f60
Thiago Reis <reist36@gmail.com>
2023-05-02 21:47:52 -0300

    correct view bug and adding the current build

--- imprimeServices/src/app/screens/home-screen/home-screen.component.ts
+++ imprimeServices/src/app/screens/home-screen/home-screen.component.ts
@@ -10,6 +10,12 @@ export class HomeScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const btnTopo = document.getElementById("btn-topo") as HTMLButtonElement;
+
+    btnTopo.onclick = function () {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    };
   }
 
 }
